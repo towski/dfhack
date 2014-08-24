5492107028abc6a465a6af77e4535eca69df5376
Matthew Wastrodowski <towski@gmail.com>
2014-08-24 12:57:07 -0700

    ruby script for handling mandates, and submitting new orders with lua with a job string and material type

---
+++ scripts/automandate.rb
@@ -0,0 +1,116 @@
+class AutoMandate
+  def initialize
+    @thresholds = Hash.new(50)
+    @lastcounts = Hash.new(0)
+    @mandates = Hash.new
+  end
+
+  def setdefault(v)
+    @thresholds.default = v.to_i
+  end
+
+  def find_mandates
+    mandates = df.world.mandates.select do |mandate|
+      mandate.mode == 1
+    end
+    @before_jobs = df.world.manager_orders.map{|o| o._memaddr }
+    @mandates.each do |key, value|
+      if !@before_jobs.include?(value)
+        puts "Removing mandate #{key} with job #{value}"
+        @mandates.delete key
+      end
+    end
+    mandates.each do |mandate|
+      if @mandates[mandate.timeout_limit.to_s].nil?
+        puts "submitting mandate for #{mandate.timeout_limit}"
+        material = nil
+        type = mandate.item_type.to_s
+        case mandate.item_type
+        when :AMULET, :RING
+          material = "rock"
+          type = "crafts"
+        when :BACKPACK
+          material = "leather"
+        else
+          material = "rock"
+        end
+        command =  "submit_order #{type} #{mandate.timeout_limit} #{material}"
+        puts command
+        df.dfhack_run command
+      end
+    end
+  end
+
+  def update(mandate_id)
+    after_jobs = df.world.manager_orders.map{|o| o._memaddr }
+    jobs = after_jobs - @before_jobs
+    @before_jobs = after_jobs
+    if jobs.length == 1
+      puts "setting job #{mandate_id} #{jobs.first}"
+      @mandates[mandate_id] = jobs.first
+    end
+  end
+
+  def process
+    puts "Finding mandates"
+    find_mandates
+    return unless @running
+  end
+
+  def start
+    return if @running
+    @onupdate = df.onupdate_register('automandate', 1200) { process }
+    @running = true
+  end
+
+  def stop
+    df.onupdate_unregister(@onupdate)
+    @running = false
+  end
+
+  def status
+    stat = @running ? "Running." : "Stopped."
+    @mandates.each { |k,v|
+      stat << "mandate #{k} : #{v}"
+    }
+    stat
+  end
+end
+
+$AutoMandate ||= AutoMandate.new
+
+case $script_args[0]
+when 'start', 'enable'
+  $AutoMandate.start
+  puts $AutoMandate.status
+
+when 'update'
+  $AutoMandate.update $script_args[1]
+
+when 'end', 'stop', 'disable'
+  $AutoMandate.stop
+  puts 'Stopped.'
+
+when 'default'
+  $AutoMandate.setdefault($script_args[1])
+
+when 'status'
+  puts $AutoMandate.status()
+
+when 'delete'
+  $AutoMandate.stop
+  $AutoMandate = nil
+
+when 'help', '?'
+  puts <<EOS
+Automatically fulfill nobles mandates
+
+Usage:
+ automandate start
+ automandate default 30
+EOS
+
+else
+  $AutoMandate.process
+  puts $AutoMandate.status
+end
---
+++ scripts/submit_order.lua
@@ -0,0 +1,77 @@
+local gui = require "gui"
+local utils = require "utils"
+local table = require "table"
+local stockflow = require 'plugins.stockflow'
+
+materials = {
+    rock = {
+        adjective = "rock",
+        management = {mat_type = 0},
+    },
+}
+
+for _, name in ipairs{"wood", "cloth", "leather", "silk", "yarn", "bone", "shell", "tooth", "horn", "pearl"} do
+    materials[name] = {
+        adjective = name,
+        management = {material_category = {[name] = true}},
+    }
+end
+
+materials.wood.adjective = "wooden"
+materials.tooth.adjective = "ivory/tooth"
+materials.leather.clothing_flag = "LEATHER"
+
+print(materials['rock'])
+
+MAX_JOB_ID = 231
+
+function getJobType(tbl, value)
+  local i = 0
+  while (i>=0 and i<=MAX_JOB_ID) do
+    v = tbl[i]
+    if v.caption then
+      if string.find(v.caption:lower(), value) then
+        return i
+      end
+    end
+    i = i + 1
+  end
+
+  return nil
+end
+
+args = {...}
+opt = args[1]
+mandate_id = args[2]
+material = args[3] or 'stone'
+print(opt)
+print(mandate_id)
+if opt == nil then
+  print("First parameter must be a string for job creation")
+  return
+end
+if material == nil then
+  print("Needs material")
+end
+
+local job = getJobType(df.job_type.attrs, opt:lower())
+if job == nil or job == 0 then
+  print("No job found for '" .. opt .."'")
+  return
+end
+
+local new_order = df.manager_order:new()
+    new_order:assign{
+        job_type = job,
+        unk_2 = -1,
+        item_subtype = -1,
+        mat_type = -1,
+        mat_index = -1,
+    }
+
+new_order:assign(materials[material].management)
+new_order.amount_left = 1
+new_order.amount_total = 1
+new_order.is_validated = 0
+df.global.world.manager_orders:insert('#', new_order)
+dfhack.run_command("automandate update " .. mandate_id)

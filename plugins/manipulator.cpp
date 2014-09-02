751f8b00c91f217f2f074ec890a8b284ed60e3e8
Matthew Wastrodowski <towski@gmail.com>
2014-09-01 23:43:05 -0700

    Revert "merge branch store"
    This reverts commit fa7503ffd982dc4fb06f1a0933ddbe98e47d787d, reversing
    changes made to bad1cd50459e2b4dcf269f6d59396194207737cf.

--- plugins/manipulator.cpp
+++ plugins/manipulator.cpp
@@ -277,7 +277,7 @@ enum altsort_mode {
     ALTSORT_MAX
 };
 
-altsort_mode old_altsort = ALTSORT_NAME; 
+altsort_mode old_altsort = ALTSORT_MAX; 
 int old_sel_column = 0;
 
 bool descending;
@@ -463,11 +463,13 @@ viewscreen_unitlaborsst::viewscreen_unitlaborsst(vector<df::unit*> &src, int cur
     sel_column = old_sel_column;
 
     refreshNames();
-    sortUnits(old_altsort);
-    if(old_altsort == ALTSORT_SKILL){
-        altsort = ALTSORT_NAME; 
-    } else {
-        altsort = old_altsort; 
+    if(old_altsort != ALTSORT_MAX){
+        sortUnits(old_altsort);
+        if(old_altsort == ALTSORT_SKILL){
+            altsort = ALTSORT_NAME; 
+        } else {
+            altsort = old_altsort; 
+        }
     }
 
     first_row = 0;
